6b7da54f3711bcff905117e7eae0a87a55df8aef
Ryan Morley <339496036@GoTVDSB.ca>
2025-02-05 14:41:42 -0500

    Still working on

--- Program.cs
+++ Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            string firstName, greetings, favMovie, capMovie;
+
+
+            
+            Console.WriteLine("What is your name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine();
+            greetings = ($"Greetings and salutations {firstName} ");
+
+            Console.WriteLine(greetings.ToLower());
+
+            Console.WriteLine("What is your favorite movie?");
+            favMovie = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"{favMovie} Nice choice");
+            capMovie = (favMovie.ToUpper());
+            Console.WriteLine(capMovie.Contains("THE"));
+            Console.WriteLine($"If you replace the letter A with @ and E with 3 {favMovie} would look like...");
+            Console.WriteLine(capMovie.Replace("A","@").Replace("E", "3"));
+            */
+
+
+            /* String favQuote;
+             favQuote = "Fred Durst are you here? Fred durst where are you?";
+             Console.WriteLine($"My favorite quote is {favQuote}");
+             Console.WriteLine($"My favorite quote is {favQuote.ToUpper().Replace("A" , " " ).Replace("E", " ").Replace("I"," ").Replace("O", " ").Replace("U", " ")}");
+            */
+
+
+            String monkey1, monkey2;
+            monkey1 = "    || __   ||\r\n    ||=\\_`\\=||\r\n    || (__/ ||\r\n    ||  | | :-\"\"\"-.\r\n    ||==| \\/-=-.   \\\r\n    ||  |(_|o o/   |_\r\n    ||   \\/ \"  \\   ,_)\r\n    ||====\\ ^  /__/\r\n    ||     ;--'  `-.\r\n    ||    /      .  \\\r\n    ||===;        \\  \\\r\n    ||   |         | |\r\n    || .-\\ '     _/_/\r\n    |:'  _;.    (_  \\\r\n    /  .'  `;\\   \\\\_/\r\n   |_ /     |||  |\\\\\r\n  /  _)=====|||  | ||\r\n /  /|      ||/  / //\r\n \\_/||      ( `-/ ||\r\n    ||======/  /  \\\\ .-.\r\n    ||      \\_/    \\'-'/\r\n    ||      ||      `\"`\r\n    ||======||\r\n    ||      ||";
+            Console.WriteLine($"{monkey1}");
+            monkey2 = "               .-\"\"\"-.\r\n             _/-=-.   \\\r\n            (_|a a/   |_\r\n             / \"  \\   ,_)\r\n        _    \\`=' /__/\r\n       / \\_  .;--'  `-.\r\n       \\___)//      ,  \\\r\n        \\ \\/;        \\  \\\r\n         \\_.|         | |\r\n          .-\\ '     _/_/\r\n        .'  _;.    (_  \\\r\n       /  .'   `\\   \\\\_/\r\n      |_ /       |  |\\\\\r\n     /  _)       /  / ||\r\n    /  /       _/  /  //\r\n    \\_/       ( `-/  ||\r\n              /  /   \\\\ .-.\r\n              \\_/     \\'-'/\r\n                       `\"`";
+            Console.Write($"{monkey2}");
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
